30666fc90883e1c01d8e3ac22f418455f6b754f4
SewerSalmon <bj.jwalker2004@gmail.com>
2022-04-01 19:10:01 +0100

    just added it so can test through main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,13 @@ public class Main{
     public static void main(String[] args)  {
        Biomes b = new Biomes();
        b.SetFound(1,"help");
+       Grid g = new Grid();
+       g.createGid();
+
+       g.move();
+       g.move();
+       g.move();
+
     }
 
 }
